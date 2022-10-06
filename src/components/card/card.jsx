91f2400895ca09281c40102f184ebf6113fbc047
Alexander <sashashvetsov3@gmail.com>
2022-10-06 14:18:08 +0300

    add cards

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -4,9 +4,9 @@ import styles from './card.module.css'
 export const Card = ({ png, name, population, region, capital }) => {
    return (
       <div className={styles.container}>
-         <img src={png} alt={name} />
+         <img className={styles.img} src={png} alt={name} />
          <div className={styles.info}>
-            <h2 className={styles.title}></h2>
+            <h2 className={styles.title}>{name}</h2>
             <p className={styles.population}>Population: {population}</p>
             <p className={styles.region}>Region: {region}</p>
             <p className={styles.capital}>Capital: {capital}</p>
--- src/components/cards/cards.jsx
+++ src/components/cards/cards.jsx
@@ -1,18 +1,19 @@
 import React from 'react'
 import { useSelector } from 'react-redux'
-import { Card } from '../card/card'
+import { Card } from '../card/card';
+import styles from './cards.module.css'
 
 
 export const Cards = () => {
    const countries = useSelector(state => state.countryReducer.country)
 
    return (
-      <>
+      <div className={styles.container}>
          {countries.map(country => {
             return (
-               <Card key={country.name} png={country.flags.png} name={country.name} population={country.population} region={country.region} capital={country.capital} />
+               <Card key={country.name} png={country.flags.png} name={country.name} population={country.population} region={country.region} capital={country.capital}/>
             )
          })}
-      </>
+      </div>
    )
 }
